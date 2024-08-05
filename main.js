3c7d22c8e7e42ee7cd09673670fc38725808e3af
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 03:42:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// part 29 // question// 43
+//pizza names
+var pizzas = ["suprem", "pepperoni", "margherita"];
+console.log("\npizza names;\n\n");
+for (var i = 0; i < pizzas.length; i++) {
+    console.log(pizzas[i]);
+    console.log("I like " + pizzas[i] + " pizza");
+}
+console.log("\nI really like pizza\n");
---
+++ main.ts
@@ -0,0 +1,13 @@
+// part 29 // question// 34
+//pizza names
+
+let pizzas: string[] = ["suprem","pepperoni","margherita"]
+
+console.log("\npizza names;\n\n")
+
+for(let i=0; i<pizzas.length; i++){
+    console.log(pizzas[i])
+    console.log("I like " + pizzas[i] + " pizza")
+    
+}
+console.log("\nI really like pizza\n")
